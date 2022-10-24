a51ec4265460f9e2e5288e6c76aef7cc7a784415
newit-sungdv <sungdv@newit.co.jp>
2022-10-24 17:27:34 +0900

    UDP | quickReply

--- src/controllers/line.controller.ts
+++ src/controllers/line.controller.ts
@@ -21,6 +21,7 @@ import {
 
 import { createLineProfile, getSuperiorUsers } from '../repositories/line_bot.repository';
 import { createMessage } from '../repositories/message.repository';
+import { User } from 'src/entify/user.entity';
 
 @Route('line')
 export default class LineController extends Controller {
@@ -126,6 +127,7 @@ export default class LineController extends Controller {
 
     switch (event.type) {
       case 'follow': // LINE Official Account is added as a friend
+        // eslint-disable-next-line no-case-declarations
         const lineProfile = await client.getProfile(lineId);
 
         console.log(JSON.stringify(lineProfile));
@@ -139,43 +141,124 @@ export default class LineController extends Controller {
         await createMessage(message);
 
         if (message.text == 'lineid') {
-          const textMessage: TextMessage = { type: 'text', text: lineId };
-          return client.replyMessage(event.replyToken, textMessage);
+          return this.replyClientId(client, event.replyToken, lineId);
         }
 
         // quick reply
-        if (message.text === '完了しております' || message.text === 'すみません、遅れております') {
+        if (message.text === '完了しております') {
           const superiorUsers = await getSuperiorUsers(lineId);
           superiorUsers.map((superiorUser) => {
-            const reportMessage: FlexMessage = {
-              type: 'flex',
-              altText: superiorUser.name + 'からのレポート\n',
-              contents: {
-                type: 'bubble',
-                body: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: '報告者：' + superiorUser.name + '\n',
-                    },
-                    {
-                      type: 'text',
-                      text: 'レポート内容：' + message.text + '\n',
-                      wrap: true,
-                    },
-                  ],
-                },
-              },
-            };
+            this.replyDoneAction(client, event.replyToken, superiorUser.name);
+            this.sendSuperiorMessage(client, superiorUser, message.text);
+          });
+        }
 
-            return client.pushMessage(superiorUser.line_id, reportMessage);
+        if (message.text === 'すみません、遅れております') {
+          this.replyDeplayAction(client, event.replyToken);
+
+          const superiorUsers = await getSuperiorUsers(lineId);
+          superiorUsers.map((superiorUser) => {
+            this.sendSuperiorMessage(client, superiorUser, message.text);
           });
         }
         break;
     }
 
     return;
+  }
+
+  private async replyClientId(client: Client, replyToken: string, lineId: string) {
+    const textMessage: TextMessage = { type: 'text', text: lineId };
+    return client.replyMessage(replyToken, textMessage);
+  }
+
+  private async replyDoneAction(client: Client, replyToken: string, superior: string) {
+    const replyMessage: FlexMessage = {
+      type: 'flex',
+      altText: '当いただき担、ありがとうございます\n',
+      contents: {
+        type: 'bubble',
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: '完了しているんですね😌\n\n',
+            },
+            {
+              type: 'text',
+              text: 'お疲れさまでした！\n',
+              wrap: true,
+            },
+            {
+              type: 'text',
+              text: '当いただき担、ありがとうございます😭\n\n',
+              wrap: true,
+            },
+            {
+              type: 'text',
+              text: '{{上長(' + superior + ')}}さんに報告しておきますね💪\n',
+              wrap: true,
+            },
+          ],
+        },
+      },
+    };
+
+    return client.replyMessage(replyToken, replyMessage);
+  }
+
+  private async replyDeplayAction(client: Client, replyToken: string) {
+    const replyMessage: FlexMessage = {
+      type: 'flex',
+      altText: '当いただき担、ありがとうございます\n',
+      contents: {
+        type: 'bubble',
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: '承知しました😖\n',
+            },
+            {
+              type: 'text',
+              text: '引き続きよろしくお願いします💪\n',
+            },
+          ],
+        },
+      },
+    };
+
+    return client.replyMessage(replyToken, replyMessage);
+  }
+
+  private async sendSuperiorMessage(client: Client, superiorUser: User, reportContent: string) {
+    const reportMessage: FlexMessage = {
+      type: 'flex',
+      altText: superiorUser.name + 'からのレポート\n',
+      contents: {
+        type: 'bubble',
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: '報告者：' + superiorUser.name + '\n',
+            },
+            {
+              type: 'text',
+              text: 'レポート内容：' + reportContent + '\n',
+              wrap: true,
+            },
+          ],
+        },
+      },
+    };
+
+    return client.pushMessage(superiorUser.line_id, reportMessage);
   }
 }
