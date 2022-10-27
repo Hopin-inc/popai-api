a18b34c14486f1d1d94f804c81331d3fd57de6bd
ThongDV <thongdv@newit.co.jp>
2022-10-27 18:11:54 +0700

    [Angle] Remove log file

--- src/logger/winston.ts
+++ src/logger/winston.ts
@@ -1,14 +1,5 @@
 import winston from 'winston';
-import 'winston-daily-rotate-file';
 import path from 'path';
-
-const transport = new winston.transports.DailyRotateFile({
-  filename: path.join(__dirname, 'logs/application-%DATE%.log'),
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '7d',
-});
 
 export default winston.createLogger({
   format: winston.format.combine(
@@ -25,5 +16,8 @@ export default winston.createLogger({
       return `[${log.timestamp}] [${log.level}] ${log.message}`;
     })
   ),
-  transports: [transport],
+  transports: [
+    // display by console
+    new winston.transports.Console(),
+  ],
 });
