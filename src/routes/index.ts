43c6ca0d0675969d5db7d0bcf5fb9bb8b0844e21
newit-sungdv <sungdv@newit.co.jp>
2022-11-09 20:52:57 +0900

    UDP | restrict access api from internet

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,9 @@
 import express from 'express';
 import { validationError } from './../middleware/validate';
-import PingController from '../controllers/ping.controller';
 import LineRouter from './line.router';
 import RemindRoute from './remind.router';
 
 const router = express();
-
-router.get('/ping', async (_req, res) => {
-  const controller = new PingController();
-  const response = await controller.getMessage();
-  return res.send(response);
-});
 
 router.use('/line', LineRouter);
 router.use('/remind', RemindRoute);
