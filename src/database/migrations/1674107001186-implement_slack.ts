323a366474f77c297e64239688e225c668a847ff
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2023-01-20 15:22:36 +0900

    refactor: Add migration files

---
+++ src/database/migrations/1674107001186-implement_slack.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class implementSlack1674107001186 implements MigrationInterface {
+    name = 'implementSlack1674107001186'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX \`IDX_e7566dddb9af62f46aeec4bbd3\` ON \`section_labels\``);
+        await queryRunner.query(`ALTER TABLE \`sections\` ADD \`channel_id\` varchar(255) COLLATE "utf8mb4_unicode_ci" NULL`);
+        await queryRunner.query(`ALTER TABLE \`messages\` ADD \`channel_id\` varchar(255) COLLATE "utf8mb4_unicode_ci" NULL`);
+        await queryRunner.query(`ALTER TABLE \`messages\` ADD \`thread_id\` varchar(255) COLLATE "utf8mb4_unicode_ci" NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`messages\` DROP COLUMN \`thread_id\``);
+        await queryRunner.query(`ALTER TABLE \`messages\` DROP COLUMN \`channel_id\``);
+        await queryRunner.query(`ALTER TABLE \`sections\` DROP COLUMN \`channel_id\``);
+        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_e7566dddb9af62f46aeec4bbd3\` ON \`section_labels\` (\`section_id\`)`);
+    }
+
+}
