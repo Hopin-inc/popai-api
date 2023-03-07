fa0f437dac6de9a7b5b16d1b8114657ff82e68b9
naoki_sakata <naoki.sakata@hopin.co.jp>
2023-03-07 11:14:33 +0900

    add try

--- src/common/NotionPageBuilder.ts
+++ src/common/NotionPageBuilder.ts
@@ -5,6 +5,8 @@ import { TodoAppCode } from "@/consts/common";
 import { Client } from "@notionhq/client";
 import Todo from "@/entities/transactions/Todo";
 import DocumentToolUser from "@/entities/settings/DocumentToolUser";
+import logger from "@/logger/winston";
+import { LoggerError } from "@/exceptions";
 
 @Service()
 export default class NotionPageBuilder {
@@ -21,75 +23,86 @@ export default class NotionPageBuilder {
     createdAt: string,
     reportId: string,
   ): Promise<CreatePageParameters> {
-    const sentences: Array<BlockObjectRequest> = [
-      {
-        object: "block",
-        type: "heading_3",
-        heading_3: { rich_text: [{ type: "text", text: { content: "昨日やったこと" } }] },
-      },
-      { object: "block", type: "paragraph", paragraph: { rich_text: [{ type: "text", text: { content: "" } }] } },
-      {
-        object: "block",
-        type: "heading_3",
-        heading_3: { rich_text: [{ type: "text", text: { content: "今日やること" } }] },
-      }];
+    try {
+      const sentences: Array<BlockObjectRequest> = [
+        {
+          object: "block",
+          type: "heading_3",
+          heading_3: { rich_text: [{ type: "text", text: { content: "昨日やったこと" } }] },
+        },
+        { object: "block", type: "paragraph", paragraph: { rich_text: [{ type: "text", text: { content: "" } }] } },
+        {
+          object: "block",
+          type: "heading_3",
+          heading_3: { rich_text: [{ type: "text", text: { content: "今日やること" } }] },
+        }];
 
-    await Promise.all([
-      ...items.completedYesterday.map(async (cy) => sentences.splice(1, 0, await this.createTodoSentence(cy))),
-      ...items.ongoing.map(async (cy) => sentences.push(await this.createTodoSentence(cy))),
-      ...items.delayed.map(async (cy) => sentences.push(await this.createTodoSentence(cy, true))),
-    ]);
+      await Promise.all([
+        ...items.completedYesterday.map(async (cy) => sentences.splice(1, 0, await this.createTodoSentence(cy))),
+        ...items.ongoing.map(async (og) => sentences.push(await this.createTodoSentence(og))),
+        ...items.delayed.map(async (d) => sentences.push(await this.createTodoSentence(d, true))),
+      ]);
 
-    return {
-      parent: { database_id: databaseId },
-      properties: {
-        "タイトル": { title: [{ text: { content: `${user.user_name} ${createdAt}` } }] },
-        "担当者": { people: [{ object: "user", id: user.auth_key }] },
-        "日付": { date: { start: createdAt } },
-        "種類": { select: { id: reportId } },
-      },
-      children: sentences,
-    };
+      return {
+        parent: { database_id: databaseId },
+        icon: { emoji: "📝" },
+        properties: {
+          "タイトル": { title: [{ text: { content: `${user.user_name} ${createdAt}` } }] },
+          "担当者": { people: [{ object: "user", id: user.auth_key }] },
+          "日付": { date: { start: createdAt } },
+          "種類": { select: { id: reportId } },
+        },
+        children: sentences,
+      };
+    } catch (error) {
+      console.error(error);
+      logger.error(new LoggerError(error.message));
+    }
   }
 
   private async createTodoSentence(todo: Todo, isDelayed?: boolean): Promise<BlockObjectRequest> {
-    switch (todo.todoapp.todo_app_code) {
-      case TodoAppCode.NOTION:
-        if (isDelayed) {
-          await this.notionRequest.pages.update(
-            {
-              page_id: todo.todoapp_reg_id,
-              icon: { type: "emoji", emoji: "🚨" },
-            });
-        }
-        return {
-          object: "block", type: "bulleted_list_item",
-          bulleted_list_item: {
-            rich_text: [{ type: "mention", mention: { page: { id: todo.todoapp_reg_id } } }],
-          },
-        };
-      case TodoAppCode.TRELLO || TodoAppCode.MICROSOFT:
-        if (isDelayed) {
+    try {
+      switch (todo.todoapp.todo_app_code) {
+        case TodoAppCode.NOTION:
+          // if (isDelayed) {
+          //   await this.notionRequest.pages.update(
+          //     {
+          //       page_id: todo.todoapp_reg_id,
+          //       icon: { type: "emoji", emoji: "🚨" },
+          //     });
+          // }
           return {
             object: "block", type: "bulleted_list_item",
             bulleted_list_item: {
-              rich_text: [{ type: "text", text: { content: "🚨" } }, {
-                type: "text",
-                text: { content: todo.name, link: { url: todo.todoapp_reg_url } },
-              }],
+              rich_text: [{ type: "mention", mention: { page: { id: todo.todoapp_reg_id } } }],
             },
           };
-        } else {
-          return {
-            object: "block", type: "bulleted_list_item",
-            bulleted_list_item: {
-              rich_text: [{
-                type: "text",
-                text: { content: todo.name, link: { url: todo.todoapp_reg_url } },
-              }],
-            },
-          };
-        }
+        case TodoAppCode.TRELLO || TodoAppCode.MICROSOFT:
+          if (isDelayed) {
+            return {
+              object: "block", type: "bulleted_list_item",
+              bulleted_list_item: {
+                rich_text: [{ type: "text", text: { content: "🚨" } }, {
+                  type: "text",
+                  text: { content: todo.name, link: { url: todo.todoapp_reg_url } },
+                }],
+              },
+            };
+          } else {
+            return {
+              object: "block", type: "bulleted_list_item",
+              bulleted_list_item: {
+                rich_text: [{
+                  type: "text",
+                  text: { content: todo.name, link: { url: todo.todoapp_reg_url } },
+                }],
+              },
+            };
+          }
+      }
+    } catch (error) {
+      console.error(error);
+      logger.error(new LoggerError(error.message));
     }
   }
 }
