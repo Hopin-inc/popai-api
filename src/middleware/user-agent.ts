62191b8f21c02534979c522f478ac18513ba8513
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2023-04-06 22:54:44 +0900

    fix: get request header on /api/tasks/*

--- src/middleware/user-agent.ts
+++ src/middleware/user-agent.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from "@/common/StatusCodes";
 import { Request, Response, NextFunction } from "express";
 
 export const allowOnlyCron = (req: Request, res: Response, next: NextFunction) => {
-  const isCron = !!req.headers["X-Appengine-Cron"];
+  const isCron = req.header("X-Appengine-Cron");
   if (!isCron) {
     return res.status(StatusCodes.FORBIDDEN).send("Access restricted!");
   }
