379448c5d439b93fbf4dfc715b6a2c6f6347c2c7
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2023-04-07 16:37:11 +0900

    fix: update cors setting

--- src/routes/app/index.ts
+++ src/routes/app/index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import cors from "cors";
-import { session } from "@/middleware/session";
 import { authRequired } from "@/middleware/auth";
 import authRouter from "@/routes/app/auth";
 import accountRouter from "@/routes/app/accounts";
@@ -10,14 +8,6 @@ import userRouter from "@/routes/app/users";
 import configRouter from "@/routes/app/config";
 
 const router = express();
-router.use(session);
-if (process.env.ENV !== "LOCAL") {
-  router.set("trust proxy", 1);
-}
-router.use(cors({
-  origin: process.env.ALLOWED_ORIGINS.split(" "),
-  credentials: true,
-}));
 
 router.use("/auth", authRouter);
 router.use("/accounts", accountRouter);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,18 @@ import { validationError } from "@/middleware/validate";
 import integrationRouter from "@/routes/integrations";
 import jobRouter from "@/routes/jobs";
 import appRouter from "@/routes/app";
+import { session } from "@/middleware/session";
+import cors from "cors";
 
 const router = express();
+router.use(session);
+if (process.env.ENV !== "LOCAL") {
+  router.set("trust proxy", 1);
+}
+router.use(cors({
+  origin: process.env.ALLOWED_ORIGINS.split(" "),
+  credentials: true,
+}));
 
 router.use(validationError);
 
