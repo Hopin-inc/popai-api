32923edd2a6cbb01d30e971ee9b28118e893fcca
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2023-04-09 20:24:52 +0900

    fix: modify mistakenly merged file

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -2,3 +2,8 @@ import { StatusCodes } from "@/common/StatusCodes";
 
 export class HttpException extends Error {
   public status?: number;
+  constructor(message: string, status?: StatusCodes) {
+    super(message);
+    this.status = status;
+  }
+}
